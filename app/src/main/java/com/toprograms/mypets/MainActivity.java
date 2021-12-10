05f81307dc7321ab2f2b21111c479be623d19588
Kirill <lirikk2020@gmail.com>
2021-12-11 02:00:24 +0300

    Novigation

--- app/src/main/java/com/toprograms/mypets/MainActivity.java
+++ app/src/main/java/com/toprograms/mypets/MainActivity.java
@@ -1,14 +1,47 @@
 package com.toprograms.mypets;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new Home()).commit();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.item1:
+                        fragment = new Home();
+                        break;
+                    case R.id.item2:
+                        fragment = new Pets();
+                        break;
+                    case R.id.item3:
+                        fragment = new Reminder();
+                        break;
+                    case R.id.item4:
+                        fragment = new Map();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,fragment).commit();
+                return true;
+            }
+        });
     }
 }
