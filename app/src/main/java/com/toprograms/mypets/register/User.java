c39f9288539f8f7896f3a1a5da3c78f34e768033
Nikita <nik.safronov.2014@bk.ru>
2021-12-11 05:33:37 +0300

    fragment Home vercion #1

---
+++ app/src/main/java/com/toprograms/mypets/register/User.java
@@ -0,0 +1,15 @@
+package com.toprograms.mypets.register;
+
+class User {
+    public  String fullName, passport, email;
+
+    public  User(){
+
+    }
+
+    public User(String fullName, String passport, String email){
+        this.fullName = fullName;
+        this.passport = passport;
+        this.email = email;
+    }
+}
